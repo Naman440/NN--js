4171f833e0256e03e555a9d15be9b1d445791403
Naman Singh <jyotipushpa180@gmail.com>
2025-06-20 18:45:13 +0000

    work continuing

---
+++ 1_basics/array1.js
@@ -0,0 +1,13 @@
+const mynewArray = [7,8,9,4,5]
+
+//mynewArray.push(1)
+//console.log(mynewArray)
+
+//mynewArray.pop()
+//console.log(mynewArray)
+
+const onlyarray = [6,5,4,3,2]
+
+const onlyArray = mynewArray.join()
+console.log(typeof onlyarray);
+console.log(typeof mynewArray);
